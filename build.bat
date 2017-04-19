07990f9a120b2307685056a6080680115631602d
Igor Timurov <igor@artplant.no>
2017-04-19 18:08:06 +0300

    use pyinstaller to make one exe

---
+++ build.bat
@@ -0,0 +1,4 @@
+set PATH=C:\Programs\Python35\Scripts;%PATH%
+
+pyinstaller igorc.spec
+pause
---
+++ igorc.py
@@ -0,0 +1,7 @@
+from igor_compiler import IgorCompiler
+
+if __name__ == '__main__':
+    compiler = IgorCompiler()
+    if compiler.parse_args():
+        compiler.run()
+    print("DONE")
